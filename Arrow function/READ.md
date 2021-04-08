6bdb73b9d5eb48e4a2af1a4b043a99a88bd303ee
Pham Quoc Cuong <pqcuong97@gmail.com>
2021-04-08 15:39:11 +0700

    add Arrow function

---
+++ Arrow function/READ.md
@@ -0,0 +1,13 @@
+## Arrow function( Ham rut gon)
+```
+var add = (a,b)=>{
+    return a+b;
+}
+
+var add! = function (a, b) {
+    return a + b;
+}
+console.log(add(5,5)); //10
+console.log(add1(5, 5)); //10
+
+```
---
+++ Arrow function/index.js
@@ -0,0 +1,12 @@
+var add = (a,b)=>{
+    return a+b;
+}
+
+var add! = function (a, b) {
+    return a + b;
+}
+console.log(add(5,5)); //10
+console.log(add1(5, 5)); //10
+
+
+// arrow function: ()=>{...} thay cho cach dinh nghia or goi ham function(){...}
