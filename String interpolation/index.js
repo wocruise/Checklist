875d3e5de09ac13bc5914bdd636208526722f749
Pham Quoc Cuong <pqcuong97@gmail.com>
2021-04-08 15:11:52 +0700

    test string Interpolation

---
+++ String interpolation/index.js
@@ -0,0 +1,10 @@
+const number = 42 ;
+const message = `this is ${number}`;
+
+console.log(message);
+
+const greeting = 'Hello';
+const who = 'World';
+
+const mess = `${greeting}, ${who}!`;
+console.log(mess); // => 'Hello, World!'
