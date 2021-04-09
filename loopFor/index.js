618f80076bbe7c9c36f7c1fe8976f8fe9e023b0d
Pham Quoc Cuong <pqcuong97@gmail.com>
2021-04-09 09:32:06 +0700

    forEach()

--- loopFor/index.js
+++ loopFor/index.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+
+
 var a =[2,4,6,8,10,12];
 for(i = 0 ; i<a.length;i++){
     console.log(a[i]); // i = 2,4,6,8,10,12
@@ -11,6 +14,21 @@ for (i of a){
     console.log(i); // i =2,4,6,8,10,12
 }
 
-a.forEach((i)=>{
-    console.log(i); // i =2,4,6,8,12
-})
+function Counter() {
+    this.sum = 0;
+    this.count = 0;
+}
+Counter.prototype.add = function (array) {
+    array.forEach(function (entry) {
+        this.sum += entry;
+        ++this.count;
+    }, this);
+    
+};
+
+const obj = new Counter();
+obj.add([2, 5, 9]);
+console.log(obj.count);
+// 3
+console.log(obj.sum);
+// 16
