908128e1ca69a506d6013d70f07135aebfec8670
Pham Quoc Cuong <pqcuong97@gmail.com>
2021-04-12 15:47:39 +0700

    add async

--- Asyncawait/index.js
+++ Asyncawait/index.js
@@ -1,20 +1,16 @@
-var fetch = require('node-fetch');
-const Bluebird = require('bluebird');
-
-fetch.Promise = Bluebird;
-async function f() {
-    try {
-        fetch('https://github.com/')
-            .then(res => {
-                console.log(res.ok);
-                console.log(res.status);
-                console.log(res.statusText);
-                console.log(res.headers.raw());
-                console.log(res.headers.get('Accept-Language'));
-            });
-    } catch (err) {
-        console.log(err); // TypeError: failed to fetch
-    }
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
 }
 
-f();
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+}
+
+asyncCall();
